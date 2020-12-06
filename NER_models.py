20368f84357ad2a116cfa9f2d86fb5daffe723c1
Zilong Jiang <jiangzl.bruce@gmail.com>
2020-12-05 19:06:11 -0500

    instructions on running models

---
+++ NER_models.py
@@ -0,0 +1,38 @@
+import nltk
+from nltk.tag import pos_tag
+from nltk.tokenize import word_tokenize
+
+# sample text and tokenized text
+text = 'Qiaotong Wu, Bruce Jiang, and Thomas Lin are students at New York University working on NER taggers.'
+tok_words = word_tokenize(text)
+
+
+print('\n\n\nStanford:\n\n\n')
+
+# Stanford Core Tagger
+
+# the stanford model takes three parameters to init, first one is the classifier (the path to a file in the classfiers directory), 
+# the second is the stanford-ner.jar file in the package (the path to the stanford-ner.jar file)
+# the third is the encoding info, in this case we just pass in utf8
+
+# to run the stanfordNERTagger, init one instance, call it "tagger", tokenize the text, and run tagger.tag(the tokenized text)
+
+from nltk.tag import StanfordNERTagger
+
+model = 'Downloads/stanford-ner-2020-11-17/classifiers/english.all.3class.distsim.crf.ser.gz'
+jar = 'Downloads/stanford-ner-2020-11-17/stanford-ner.jar'
+encoding = 'utf8'
+
+tagger = StanfordNERTagger(model, jar, encoding)
+print(tagger.tag(tok_words))
+
+print('\n\n\nNLTK:\n\n\n')
+
+# NLTK NE chunking
+
+# to run NLTK Named Entity chunking, we first tokenize the text, then do POS tagging on the tokenized text with nltk.pos_tag(), then we run nltk.ne_chunk()
+# on the tagged text.
+
+# notice that what is returned is a tree-liked annotated text, so we have to handle the formatting to match with the answer key
+pos_tags = pos_tag(tok_words)
+print(nltk.ne_chunk(pos_tags))
