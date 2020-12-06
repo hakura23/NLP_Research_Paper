e0681999e56e677b5f8f231282bcfed5d02852bc
Zilong Jiang <jiangzl.bruce@gmail.com>
2020-12-06 03:06:58 -0500

    updated Flair instruction

--- NER_models.py
+++ NER_models.py
@@ -11,6 +11,7 @@ print('\n\n\nStanford:\n\n\n')
 
 # Stanford Core Tagger
 
+# no need to download the stanford software, I have included the software in the current dictory, stanford-ner-2020-11-17
 # the stanford model takes three parameters to init, first one is the classifier (the path to a file in the classfiers directory), 
 # the second is the stanford-ner.jar file in the package (the path to the stanford-ner.jar file)
 # the third is the encoding info, in this case we just pass in utf8
@@ -37,23 +38,56 @@ print('\n\n\nNLTK:\n\n\n')
 pos_tags = pos_tag(tok_words)
 print(nltk.ne_chunk(pos_tags))
 
-#Flair NE
-import flair
-tagger = SequenceTagger.load('ner') #takes very long to load
-sentence = Sentence('George Washington went to Washington .')
+
+
+print('\n\n\nFlair:\n\n\n')
+
+# Flair
+
+# first we need to download flair software, run the following command:
+# pip3 install flair
+# or pip install flair (if you are using pip instead of pip3)
+
+# now we can use flair to do NER tagging
+from flair.data import Sentence
+from flair.models import SequenceTagger
+
+# first we load the NER tagger, this should be done only once
+tagger = SequenceTagger.load('ner')
+
+# call Sentence() with the text passed in to create Sentence object
+sentence = Sentence(text)
+
+# predict NER tags
 tagger.predict(sentence)
-print(sentence.to_tagged_string())
-#This should print: "George <B-PER> Washington <E-PER> went to Washington <S-LOC> ."
+
+# print results
+# word + \t + 
+for token in sentence:
+    print(str(token).split(' ')[2], end='')
+    print('\t', end='')
+    print(str(token.get_tag('ner')).split(' ')[0])
 
 
-#spaCy NE
-import spacy
-nlp = spacy.load("en_core_web_sm")
 
-doc = nlp('Qiaotong\n Wu\n, Bruce\n Jiang\n, and\n Thomas\n Lin\n are\n students\n at\n New\n York\n University\n working\n on\n NER\n taggers\n.\n')
-#We can just use the label data field to see the tag
-for ent in doc.ents:
-	    print(ent.text, ent.start_char, ent.end_char, ent.label_)
+# SpaCY still needs improvement on output format
+# print('\n\n\nSpaCY\n\n\n')
+
+# #spaCy NE
+
+# # same as above, first thing to do: pip3 install spacy
+# # then, run: python3 -m spacy download en_core_web_sm
+
+# # import spacy
+# import spacy
+
+# # load the tagger
+# tagger = spacy.load("en_core_web_sm")
+
+# doc = tagger(text)
+# # we can just use the label data field to see the tag
+# for ent in doc.ents:
+#     print(ent.text, ent.start_char, ent.end_char, ent.label_)
 
 # Wu 10 13 PERSON
 # Bruce 15 20 PERSON
@@ -64,7 +98,7 @@ for ent in doc.ents:
 #  University 72 88 ORG
 
 
-#Polyglot
+# #Polyglot
 
 
 
