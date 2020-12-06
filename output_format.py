493059d626757cb64620fc350de0b5f851a08a3d
吴巧曈 <wuqt@Hakura-2.local>
2020-12-06 15:08:35 -0500

    correct output_format

--- output_format.py
+++ output_format.py
@@ -17,7 +17,7 @@ def stanford_format(list_of_taggings):
 
 
 # spaCy
-def spaCy_format(list_of_taggings):
+def spacy_format(list_of_taggings):
     out_str = '\n'
     length = len(list_of_taggings)
     for i in range(length):
@@ -52,4 +52,22 @@ def flair_format(list_of_taggings):
     out_str = out_str + '\n'
     return out_str
 
-# NLTK
+# Format for NLTK
+def nltk_format(tag_tree):
+    list_of_taggings = tree2conlltags(tag_tree)
+    out_str = '\n'
+    length = len(list_of_taggings)
+    for i in range(length):
+        tag_tuple = list_of_taggings[i]
+        if 'PERSON' in tag_tuple[2]:
+            out_str = out_str + tag_tuple[0] + '\tPER\n'
+        elif 'ORGANIZATION' in tag_tuple[2]:
+            out_str = out_str + tag_tuple[0] + '\tORG\n'
+        elif 'LOCATION' in tag_tuple[2]:
+            out_str = out_str + tag_tuple[0] + '\tLOC\n'
+        elif 'GPE' in tag_tuple[2]:
+            out_str = out_str + tag_tuple[0] + '\tLOC\n'
+        else:
+            out_str = out_str + tag_tuple[0] + '\tO\n'
+    out_str = out_str + '\n'
+    return out_str
