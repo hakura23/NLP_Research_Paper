b3150ea44dc8f1406874910fbea6897f98104cc9
Thomas Lin <tl2378@nyu.edu>
2020-12-06 05:13:39 -0500

    formatted corpora

---
+++ WNUT17/CONLL-format/data/dev/Remover.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class Remover{
+	public static void main(String[] args){
+		try{
+			BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+	        String line = reader.readLine();
+    		File file = new File(args[0] + "TestFormatted.txt");
+      		file.createNewFile();
+      		FileWriter myWriter = new FileWriter(args[0] + "TestFormatted.txt");
+	        while (line != null){
+	        	if(line.length() != 0){
+	        		String[] curr = line.split("\t");
+	        		myWriter.write(curr[0] + "\n");
+	        	}
+	        	else{
+	        		myWriter.write("\n");
+	        	}
+	            line = reader.readLine();
+	        }
+	        reader.close();
+	        myWriter.close();
+		}
+		catch(IOException ex){
+        	System.out.println(ex.getMessage());
+		}
+	}
+	    
+}
